55b38c550c5fefa3c6d84bb02cf9bcad25382cb8
Suylo <spoownz@yahoo.com>
2023-05-23 11:31:55 +0200

    Début Projet Web Django

---
+++ webprojet/appliprojet/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+
+class Categorie(models.Model):
+    id_categorie = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=50, verbose_name='Nom de la catégorie')
+
+    def __str__(self):
+        return self.nom
+
+
+class Selection(models.Model):
+    id_selection = models.AutoField(primary_key=True)
+    titre = models.CharField(max_length=50, verbose_name='Titre de la sélection')
+
+    def __str__(self):
+        return self.titre
+
+
+class Jeux(models.Model):
+    id_jeux = models.AutoField(primary_key=True)
+    label = models.CharField(max_length=50, verbose_name='Nom du jeu')
+    img_url = models.CharField(max_length=255, verbose_name='Url image')
+    prix = models.FloatField(verbose_name='Prix du jeu')
+    selection = models.ForeignKey(Selection, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.label
+
+
+class Posseder(models.Model):
+    jeux = models.ForeignKey(Jeux, on_delete=models.CASCADE)
+    categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Jeux: {self.jeux.label} | Categorie: {self.categorie.nom}"
