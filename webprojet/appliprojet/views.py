d47c470e2e13ea5fd9057a5e86a23396d63b20bb
Suylo <spoownz@yahoo.com>
2023-05-23 11:59:21 +0200

    Projet django webapp

--- webprojet/appliprojet/views.py
+++ webprojet/appliprojet/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from appliprojet.models import Jeux
+
+
 # Create your views here.
+def jeux(request):
+
+    jeux = Jeux.objects.all()
+
+    return render(request, 'appliprojet/jeux.html', {'jeux': jeux})
